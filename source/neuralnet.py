e5d1dfbc01997f35c9f774027a7b4164826a6297
YeongHyeon <young200405@gmail.com>
2019-12-02 19:55:51 +0900

    neuralnet

--- source/neuralnet.py
+++ source/neuralnet.py
@@ -12,30 +12,64 @@ class CVAE(object):
         self.name_bank, self.var_bank = [], []
 
         self.encoder(tf.zeros([1, self.height, self.width, self.channel]))
+        self.decoder(tf.zeros([1, self.z_dim]))
 
     def encoder(self, x):
 
         x = tf.cast(x, dtype=tf.float32)
-        conv1_1 = self.conv2d(x, self.get_weight(vshape=[self.ksize, self.ksize, 1, 16], name="conv1_1"), \
-            stride_size=1, padding='SAME')
-        conv1_2 = self.conv2d(conv1_1, self.get_weight(vshape=[self.ksize, self.ksize, 16, 16], name="conv1_2"), \
-            stride_size=1, padding='SAME')
+        conv1_1 = self.conv2d(x, self.get_weight(vshape=[self.ksize, self.ksize, self.channel, 16], name="enc_conv1_1"), \
+            stride_size=1, padding='SAME', activation='elu')
+        conv1_2 = self.conv2d(conv1_1, self.get_weight(vshape=[self.ksize, self.ksize, 16, 16], name="enc_conv1_2"), \
+            stride_size=1, padding='SAME', activation='elu')
         maxp1 = self.maxpool(conv1_2, pool_size=2, stride_size=2)
 
-        conv2_1 = self.conv2d(maxp1, self.get_weight(vshape=[self.ksize, self.ksize, 16, 32], name="conv2_1"), \
-            stride_size=1, padding='SAME')
-        conv2_2 = self.conv2d(conv2_1, self.get_weight(vshape=[self.ksize, self.ksize, 32, 32], name="conv2_2"), \
-            stride_size=1, padding='SAME')
+        conv2_1 = self.conv2d(maxp1, self.get_weight(vshape=[self.ksize, self.ksize, 16, 32], name="enc_conv2_1"), \
+            stride_size=1, padding='SAME', activation='elu')
+        conv2_2 = self.conv2d(conv2_1, self.get_weight(vshape=[self.ksize, self.ksize, 32, 32], name="enc_conv2_2"), \
+            stride_size=1, padding='SAME', activation='elu')
         maxp2 = self.maxpool(conv2_2, pool_size=2, stride_size=2)
 
-        conv3_1 = self.conv2d(maxp2, self.get_weight(vshape=[self.ksize, self.ksize, 32, 64], name="conv3_1"), \
-            stride_size=1, padding='SAME')
-        conv3_2 = self.conv2d(conv3_1, self.get_weight(vshape=[self.ksize, self.ksize, 64, 64], name="conv3_2"), \
-            stride_size=1, padding='SAME')
-        maxp3 = self.maxpool(conv3_2, pool_size=2, stride_size=2)
+        conv3_1 = self.conv2d(maxp2, self.get_weight(vshape=[self.ksize, self.ksize, 32, 64], name="enc_conv3_1"), \
+            stride_size=1, padding='SAME', activation='elu')
+        conv3_2 = self.conv2d(conv3_1, self.get_weight(vshape=[self.ksize, self.ksize, 64, 64], name="enc_conv3_2"), \
+            stride_size=1, padding='SAME', activation='elu')
 
-        dense
-        
+        in_dense = (self.height//(2**2))*(self.width//(2**2))*64
+        flatten = tf.reshape(conv3_2, shape=(-1, in_dense))
+        fullcon1 = self.dense(flatten, self.get_weight(vshape=[in_dense, 512], name="enc_fullcon1"), \
+            activation='elu')
+        fullcon2 = self.dense(fullcon1, self.get_weight(vshape=[512, self.z_dim*2], name="enc_fullcon2"), \
+            activation='none')
+
+        return fullcon2
+
+    def decoder(self, z):
+
+        z = tf.cast(z, dtype=tf.float32)
+        out_dense = (self.height//(2**2))*(self.width//(2**2))*64
+        fullcon1 = self.dense(z, self.get_weight(vshape=[self.z_dim, 512], name="dec_fullcon1"), \
+            activation='elu')
+        fullcon2 = self.dense(fullcon1, self.get_weight(vshape=[512, out_dense], name="dec_fullcon2"), \
+            activation='elu')
+        spatial = tf.reshape(fullcon2, shape=(-1, self.height//(2**2), self.width//(2**2), 64))
+
+        conv1_1 = self.conv2d(spatial, self.get_weight(vshape=[self.ksize, self.ksize, 64, 64], name="dec_conv1_1"), \
+            stride_size=1, padding='SAME', activation='elu')
+        conv1_2 = self.conv2d(conv1_1, self.get_weight(vshape=[self.ksize, self.ksize, 64, 32], name="dec_conv1_2"), \
+            stride_size=1, padding='SAME', activation='elu')
+
+        conv2_1 = self.conv2d_tr(conv1_2, self.get_weight(vshape=[self.ksize, self.ksize, 32, 32], name="dec_conv2_1"), output_shape=[tf.shape(z)[0], self.height//(2**1), self.width//(2**1), 32], \
+            stride_size=2, padding='SAME', activation='elu')
+        conv2_2 = self.conv2d(conv2_1, self.get_weight(vshape=[self.ksize, self.ksize, 32, 16], name="dec_conv2_2"), \
+            stride_size=1, padding='SAME', activation='elu')
+
+        conv3_1 = self.conv2d_tr(conv2_2, self.get_weight(vshape=[self.ksize, self.ksize, 16, 16], name="dec_conv3_1"), output_shape=[tf.shape(z)[0], self.height//(2**0), self.width//(2**0), 16], \
+            stride_size=2, padding='SAME', activation='elu')
+        conv3_2 = self.conv2d(conv3_1, self.get_weight(vshape=[self.ksize, self.ksize, 16, self.channel], name="dec_conv3_2"), \
+            stride_size=1, padding='SAME', activation='none')
+
+        return fullcon2
+
 
     def variable_maker(self, var_bank, name_bank, shape, name=""):
 
@@ -69,20 +103,44 @@ class CVAE(object):
         print(name, w.shape)
         return w, b
 
-    def conv2d(self, inputs, variables, stride_size, padding):
+    def activation_fn(self, input, activation="relu", name=""):
+
+        if("sigmoid" == activation):
+            out = tf.compat.v1.nn.sigmoid(input, name='%s_sigmoid' %(name))
+        elif("tanh" == activation):
+            out = tf.compat.v1.nn.tanh(input, name='%s_tanh' %(name))
+        elif("relu" == activation):
+            out = tf.compat.v1.nn.relu(input, name='%s_relu' %(name))
+        elif("lrelu" == activation):
+            out = tf.compat.v1.nn.leaky_relu(input, name='%s_lrelu' %(name))
+        elif("elu" == activation):
+            out = tf.compat.v1.nn.elu(input, name='%s_elu' %(name))
+        else: out = input
+
+        return out
+
+    def conv2d(self, inputs, variables, stride_size, padding, activation):
 
         [weights, biasis] = variables
-        out = tf.nn.conv2d(inputs, weights, strides=[1, stride_size, stride_size, 1], \
-            padding=padding) + biasis
-        return tf.nn.elu(out)
+        out = tf.nn.conv2d(inputs, weights, \
+            strides=[1, stride_size, stride_size, 1], padding=padding) + biasis
+        return self.activation_fn(out, activation=activation)
+
+    def conv2d_tr(self, inputs, variables, output_shape, stride_size, padding, activation):
+
+        [weights, biasis] = variables
+        out = tf.nn.conv2d_transpose(inputs, weights, output_shape, \
+            strides=[1, stride_size, stride_size, 1], padding=padding) + biasis
+        return self.activation_fn(out, activation=activation)
 
     def maxpool(self, inputs, pool_size, stride_size):
 
+        print("max pool")
         return tf.nn.max_pool2d(inputs, ksize=[1, pool_size, pool_size, 1], \
             padding='VALID', strides=[1, stride_size, stride_size, 1])
 
-    def dense(self, inputs, variables):
+    def dense(self, inputs, variables, activation):
 
         [weights, biasis] = variables
         out = tf.matmul(inputs, weights) + biasis
-        return tf.nn.elu(out)
+        return self.activation_fn(out, activation=activation)
